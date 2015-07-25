1f3a0d73c1312f375c7e3c29b539b2b188ff807f
Shin-Cheng Mu <scm@iis.sinica.edu.tw>
2015-07-25 21:35:14 +0800

    Allowing weight shifting in test generation.

--- prog_new_productive/TestGen.hs
+++ prog_new_productive/TestGen.hs
@@ -5,29 +5,32 @@ import Control.Monad
 import Control.Monad.Random
 
 genElem :: MonadRandom m =>
-   (Double, Double) -> (Int, Int) -> m (Double, Int)
-genElem (vMin, vMax) (wMin, wMax) =
+   (Double, Double) -> Double -> (Int, Int) -> m (Double, Int)
+genElem (vMin, vMax) vShift (wMin, wMax) =
  do v <- getRandomR (vMin, vMax)
     w <- getRandomR (wMin, wMax)
-    return (v, w)
+    let v' = v + vShift
+    if v' > vMax then return (v, w)
+                 else return (v', w)
 
 genElems :: MonadRandom m =>
-  (Double, Double) -> (Int, Int) -> Integer -> m [(Double, Int)]
-genElems _ _ 0 = return []
-genElems (vMin, vMax) (wMin, wMax) n =
-  do e  <- genElem  (vMin, vMax) (wMin, wMax)
-     es <- genElems (vMin, vMax) (wMin, wMax) (n-1)
+  (Double, Double) -> Double -> (Int, Int) -> Integer -> m [(Double, Int)]
+genElems _ _ _ 0 = return []
+genElems (vMin, vMax) vShift (wMin, wMax) n =
+  do e  <- genElem  (vMin, vMax) vShift (wMin, wMax)
+     es <- genElems (vMin, vMax) vShift (wMin, wMax) (n-1)
      return (e : es)
 
 
 main :: IO ()
-main = do (vMin' : vMax' : wMin' : wMax' : n' : m' : _) <- getArgs
+main = do (vMin' : vMax' : vShift' : wMin' : wMax' : n' : m' : _) <- getArgs
           let vMin = read vMin'
           let vMax = read vMax'
+          let vShift = read vShift'
           let wMin = read wMin'
           let wMax = read wMax'
           let n    = read n'
           let m    = read m'
           replicateM_ m
-            (do es <- evalRandIO (genElems (vMin, vMax) (wMin, wMax) n)
+            (do es <- evalRandIO (genElems (vMin, vMax) vShift (wMin, wMax) n)
                 print es)
